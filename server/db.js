c60ffde2556ded1133d64dfab0544411bebc2696
MikhailMusikhin <mikhail.99925@gmail.com>
2025-05-13 03:51:50 +0300

    Use env vars for MySQL connection in db.js

--- server/db.js
+++ server/db.js
@@ -2,13 +2,15 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host:     'localhost',
-  user:     'root',
-  password: '',          // ваш пароль
-  database: 'restaurant_booking',
+  host:     process.env.DB_HOST,
+  port:     process.env.DB_PORT,
+  user:     process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   timezone: '+00:00'
 });
 
 module.exports = pool;
+

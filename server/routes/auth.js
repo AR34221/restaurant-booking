de102a0dcfb879986719784f64ef99b7cb3e47aa
MikhailMusikhin <mikhail.99925@gmail.com>
2025-05-13 03:24:55 +0300

    Switch from bcrypt to bcryptjs for Linux-friendly builds

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,6 @@
 // server/routes/auth.js
 const express = require('express');
-const bcrypt  = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const db      = require('../db');
 const router  = express.Router();
 
